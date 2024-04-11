f2547e02bdff993aee1d17cc307ddcf77e91a7c2
Jose Teodoro <joseateodoro.jt@gmail.com>
2024-04-11 14:37:53 -0400

    troubleshooting

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
+const Stack = createNativeStackNavigator();
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+import LoadUp from "./screens/LoadUp";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
